e4d1cbf4c580e946b0127abd97495d6c52cf4c19
Satoru Hagiwara <hagiwara@fusic.co.jp>
2020-10-07 14:33:23 +0900

    cake4 middleware関係の調整＋Errorオブジェクトが飛んできたときの対応

--- src/Middleware/EncountErrorHandlerMiddleware.php
+++ src/Middleware/EncountErrorHandlerMiddleware.php
@@ -4,14 +4,17 @@ namespace Encount\Middleware;
 
 use Cake\Error\Middleware\ErrorHandlerMiddleware;
 use Encount\Encount;
+use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Message\ServerRequestInterface;
+use Throwable;
 
 class EncountErrorHandlerMiddleware extends ErrorHandlerMiddleware
 {
-    public function handleException($exception, $request, $response)
+    public function handleException(Throwable $exception, ServerRequestInterface $request): ResponseInterface
     {
         $encount = new Encount();
         $encount->execute($exception);
 
-        return parent::handleException($exception, $request, $response);
+        return parent::handleException($exception, $request);
     }
 }
